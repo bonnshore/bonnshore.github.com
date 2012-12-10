a85b05f418c6afa2fa9a7f27d7ed34aeda23574e
bonnshore <bonnshore@gmail.com>
2012-12-11 02:07:16 +0800

    Little changes for version2.0

--- assets/js/autotag.js
+++ assets/js/autotag.js
@@ -1,7 +1,7 @@
 var autotag = {
 	init: function(){
 		var color = ['#8431cf', '#1332df', '#f00122', '#8c4211', '#de3f90', '#666', '#05d30d', '#e957ea', '#007aad', '#f00'];
-		var size = ['16px', '13px','14px','15px','17px'];
+		var size = ['16px', '13px','19px','22px','17px'];
 		$("#tagDiv").find('a').each(function(i,e){ 
 			$(e).css('color', color[autotag.rand(color.length)]);
 			$(e).css('font-size', size[autotag.rand(size.length)]);
