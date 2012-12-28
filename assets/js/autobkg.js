40077009ecbec0e7f3ea9ee935b9f8e50cf4f79b
bonnshore <bonnshore@gmail.com>
2012-12-28 23:18:44 +0800

    fix the short post bkg bug

--- assets/js/autobkg.js
+++ assets/js/autobkg.js
@@ -4,28 +4,28 @@ var autobkg = {
                 weekday=weekday.getDay()
 		switch(weekday){
 		    case 0:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body7.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body0.jpg) fixed';
 			break;
 		    case 1:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body1.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body1.jpg) fixed';
 			break;
 		    case 2:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body2.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body2.jpg) fixed';
 			break;
 		    case 3:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body3.jpg) fixed')	
+			document.body.style.background='url(/assets/themes/twitter/img/body3.jpg) fixed';
 			break;
 		    case 4:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body4.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body4.jpg) fixed';
 			break;
 		    case 5:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body5.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body5.jpg) fixed';
 			break;
 		    case 6:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body6.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body6.jpg) fixed';
 			break;
 		    default:
-			$(wrapper).css('background','url(/assets/themes/twitter/img/body1.jpg) fixed')
+			document.body.style.background='url(/assets/themes/twitter/img/body1.jpg) fixed';
 		}
 	      }
 }
