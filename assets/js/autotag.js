3732cf71ae2aba3c26de9dfd4a322250bfbf4638
bonnshore <bonnshore@gmail.com>
2013-04-03 23:41:09 +0800

    change the blog's theme back into grey

--- assets/js/autotag.js
+++ assets/js/autotag.js
@@ -1,6 +1,6 @@
 var autotag = {
 	init: function(){
-		var color = ['#8431cf', '#1332df', '#f00122', '#8c4211', '#de3f90', '#666', '#05d30d', '#e957ea', '#007aad', '#f00'];
+		var color = ['#9D9D9D', '#adadad', '#3C3C3C', '#5b5b5b', '#7b7b7b', '#272727'];
 		var size = ['16px', '25px','19px','17px'];
 		$(tagDiv).find('a').each(function(i,e){ 
 			$(e).css('color', color[autotag.rand(color.length)]);
